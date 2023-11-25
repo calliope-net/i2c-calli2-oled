c1d8dff2cb46b1a341951388abeb059f09e30ab4
calliope-net <141074597+calliope-net@users.noreply.github.com>
2023-11-25 18:21:31 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,14 @@
 radio.onReceivedNumber(function (receivedNumber) {
-	
+    funkzeit_ms = input.runningTime()
+    OLEDtext.writeText8x16(1, 0, 7, bit.formatNumber(receivedNumber, bit.eLength.HEX_FFFFFFFF))
+    i2cSchleife()
 })
+function funkTimeout () {
+    Calli2bot.i2cRESET_OUTPUTS()
+    OLEDtext.writeText8x16(1, 0, 7, oled.oled_text("TIMEOUT"))
+    Calli2bot.setLed1(calli2bot.eLed.poweron, true, true)
+    calli2bot.pause(calli2bot.calli2bot_ePause(calli2bot.ePause.p05))
+}
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
 	
 })
@@ -9,6 +17,10 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     	
     }
 })
+function i2cSchleife () {
+    Calli2bot.setLed1(calli2bot.eLed.poweron, true, false, 2)
+    alle2Sekunden()
+}
 function alle2Sekunden () {
     if (input.runningTime() - laufzeit_ms > 2000) {
         laufzeit_ms = input.runningTime()
@@ -18,6 +30,7 @@ function alle2Sekunden () {
     }
 }
 let laufzeit_ms = 0
+let funkzeit_ms = 0
 let Calli2bot: calli2bot.Calli2bot = null
 let OLEDtext: oled.oledclass = null
 OLEDtext = oled.new_oledclass(oled.oled_eADDR_OLED(oled.eADDR_OLED.OLED_16x8_x3C), false, true)
@@ -26,12 +39,18 @@ Calli2bot = calli2bot.beimStart(calli2bot.calli2bot_eADDR(calli2bot.eADDR.CB2_x2
 OLEDtext.set_eeprom_8x8(oled.oled_eEEPROM_Startadresse(oled.eEEPROM_Startadresse.F000))
 OLEDtext.writeText8x16(0, 0, 7, "TYP " + Calli2bot.i2cReadFW_VERSION(calli2bot.eVersion.Typ))
 radio.setGroup(22)
+funkzeit_ms = 1
 basic.forever(function () {
-    if (OLEDtext.geti2cError_OLED() == 0) {
-        Calli2bot.setLed1(calli2bot.eLed.poweron, true, false, 2)
-        alle2Sekunden()
-    } else {
+    if (OLEDtext.geti2cError_OLED() != 0) {
         oled.comment("im Simulator kein Programm abarbeiten")
         basic.setLedColor(0xff0000)
+    } else if (funkzeit_ms == 0) {
+        oled.comment("dauerhaft nur, wenn keine Daten über Bluetooth empfangen werden")
+        i2cSchleife()
+    } else if (input.runningTime() - funkzeit_ms > 2000) {
+        oled.comment("Timeout, wenn länger als 2s keine Daten über Bluetooth empfangen werden")
+        funkTimeout()
+    } else {
+    	
     }
 })
